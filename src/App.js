1e1223fa0e6d9e45f2b0132d59d04a4166189c29
tanroy18 <tanroy18@in.ibm.com>
2021-01-09 11:02:55 +0530

    update carbon design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Header,
+  HeaderName,
+  HeaderGlobalAction,
+  HeaderGlobalBar,
+  HeaderNavigation,
+  HeaderMenu,
+  HeaderMenuItem,
+  Button
+} from "carbon-components-react";
+import Search20 from "@carbon/icons-react/lib/search/20";
+import Notification20 from "@carbon/icons-react/lib/notification/20";
+import AppSwitcher20 from "@carbon/icons-react/lib/app-switcher/20";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Header aria-label="IBM Platform Name">
+          <HeaderName href="#" prefix="TEST">
+            [Platform]
+          </HeaderName>
+          <HeaderNavigation aria-label="IBM [Platform]">
+            <HeaderMenuItem href="#">Link 1</HeaderMenuItem>
+            <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
+            <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
+            <HeaderMenu aria-label="Link 4" menuLinkName="Link 4">
+              <HeaderMenuItem href="#">Sub-link 1</HeaderMenuItem>
+              <HeaderMenuItem href="#">Sub-link 2</HeaderMenuItem>
+              <HeaderMenuItem href="#">Sub-link 3</HeaderMenuItem>
+            </HeaderMenu>
+          </HeaderNavigation>
+          <HeaderGlobalBar>
+            <HeaderGlobalAction aria-label="Search" onClick={() => {}}>
+              <Search20 />
+            </HeaderGlobalAction>
+            <HeaderGlobalAction aria-label="Notifications" onClick={() => {}}>
+              <Notification20 />
+            </HeaderGlobalAction>
+            <HeaderGlobalAction aria-label="App Switcher" onClick={() => {}}>
+              <AppSwitcher20 />
+            </HeaderGlobalAction>
+          </HeaderGlobalBar>
+        </Header>
+
+        <Button className="custom-class">Submit</Button>
+
+
     </div>
   );
 }
